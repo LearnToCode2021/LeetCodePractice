41891beca9246d2c905c61a71b883519fae50527
LearnToCode2021 <coding61series@gmail.com>
2021-09-16 13:24:16 -0700

    SpiralMatrix complete

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -36,33 +36,75 @@ public class SpiralMatrix {
         while(rowSteps >= 0 && colSteps >= 0) {
             switch (nextDir) {
                 case RIGHT:
-                    while(col < maxCols - (maxCols - colSteps)) {
+                    int ctr = 0;
+                    while(ctr < colSteps) {
                         retList.add(matrix[row][col]);
                         col++;
+                        ctr++;
                     }
+                    col--; //decrement col counter to be in range
+
                     colSteps--;
+                    row++; // next direction should not double count last int
                     nextDir = Direction.DOWN;
                     continue;
 
                 case LEFT:
+                    ctr = 0;
+                    while (ctr < colSteps) {
+                        retList.add(matrix[row][col]);
+                        col--;
+                        ctr++;
+                    }
+                    col++;
+
+                    colSteps--;
+                    row--; //next direction should not double count
+                    nextDir = Direction.UP;
                     continue;
 
                 case UP:
+                    ctr = 0;
+                    while (ctr < rowSteps) {
+                        retList.add(matrix[row][col]);
+                        row--;
+                        ctr++;
+                    }
+                    row++;
+
+                    col++; //next direction should not double count
+                    rowSteps--;
+                    nextDir = Direction.RIGHT;
                     continue;
 
                 case DOWN:
+                    ctr = 0;
+                    while(ctr < rowSteps) {
+                        retList.add(matrix[row][col]);
+                        row++;
+                        ctr++;
+                    }
+                    row--; //decrement row counter to be in range
+
+                    rowSteps--;
+                    col--; //next direction should not include last int
+                    nextDir = Direction.LEFT;
                     continue;
             }
         }
+        return retList;
     }
 
     @Test
     public void test() {
+//        int[][] simpleTestMatrix = new int[][]{{1, 2, 3}};
+//        List<Integer> simpleTestMatrixList = List.of(1, 2, 3);
+//        assertEquals(simpleTestMatrixList, spiralOrder(simpleTestMatrix));
         int[][] matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        Integer[] testArray = new Integer[] {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
-//        List<Integer> expected = List.of(1, 2, 3, 4, 8, 12, 11 ,10, 9, 5, 6, 7);
-        List<Integer> expected = Arrays.asList(testArray);
         List<Integer> actual = spiralOrder(matrix);
+        Integer[] matrixArray = new Integer[] {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+//        List<Integer> expected = List.of(1, 2, 3, 4, 8, 12, 11 ,10, 9, 5, 6, 7);
+        List<Integer> expected = Arrays.asList(matrixArray);
         assertEquals(expected, actual);
     }
 }
